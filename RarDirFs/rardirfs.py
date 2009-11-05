e19d494c6ab0045b325cf29dbe422554c253de96
Jonas Jonsson <jonas@websystem.se>
2009-11-05 15:20:32 +0100

    Implemented support for compressed archives using unrar util.

--- RarDirFs/rardirfs.py
+++ RarDirFs/rardirfs.py
@@ -13,6 +13,7 @@ import os      # for filesystem modes (O_RDONLY, etc)
 import errno   # for error number codes (ENOENT, etc)
                # - note: these must be returned as negatives
 import traceback
+import subprocess
 import re
 import rarfile
 
@@ -51,12 +52,80 @@ def parsePatternFile(filename):
         print e
     return ret
 
+class CacheManager(object):
+    '''
+        Manage a cache of files compressed in rar archives.
+    '''
+
+    unrar_cmd = ['unrar', 'e', '-inul', '-y']
+
+    def __init__(self, path):
+        '''
+            Path should be an absolute path.
+        '''
+        object.__init__(self)
+        self.procs = {}
+        self.path = path
+
+        if not os.path.isdir(path):
+            os.makedirs(path)
+
+    def get(self, entry):
+        '''
+            Get a file-like object of a compressed file inside an archive.
+
+            Will spawn an unrar process if needed thus it's not true that the
+            file will be complete when returned.
+        '''
+        cache_dir  = os.path.join(self.path, "." + entry.realpath)
+        cache_file = os.path.join(cache_dir, entry.rar_info.filename)
+
+        if not os.path.isdir(cache_dir):
+            os.makedirs(cache_dir)
+
+        if os.path.isfile(cache_file):
+            if os.path.getsize(cache_file) == entry.rar_info.file_size:
+                return cache_file
+
+            if cache_file in self.procs:
+                ret_code = self.procs[cache_file].poll()
+                if not ret_code:
+                    # Still running or finished nicely
+                    return cache_file
+                raise IOError(errno.EIO, 'I/O error')
+
+        # Now, file is either broken or not present, anyway, start to unpack it
+        cmd = self.unrar_cmd[:]
+        cmd.append("." + entry.realpath)
+        cmd.append(entry.rar_info.filename)
+        cmd.append(cache_dir)
+        try:
+            self.procs[cache_file] = subprocess.Popen(cmd)
+            # Wait until the file is created, or a bad ret code is returned
+            wait_count = 0
+            while wait_count < 10:
+                if not os.path.isfile(cache_file):
+                    ret = self.procs[cache_file].poll()
+                    if ret:
+                        raise Exception('Unrar failed, returned: {0}'.format(ret))
+                    wait_count += 1
+                    time.sleep(0.05)
+                else:
+                    break
+        except Exception:
+            traceback.print_exc()
+            raise IOError(errno.EIO, '')
+
+        # Simply assume it worked.
+        return cache_file
+
 class VfsEntry(object):
     '''
         An entry in the vfs dictonary.
     '''
 
     def __init__(self, realpath):
+        object.__init__(self)
         self.rar = None
         self.rar_info = None
         self.realpath = realpath
@@ -97,7 +166,6 @@ class RarStat(fuse.Stat):
     '''
         Stat for a file inside a rar archive.
     '''
-    #TODO: Inherit RoStat instead?
 
     def __init__(self, filename, info):
         fuse.Stat.__init__(self)
@@ -122,6 +190,114 @@ class RarStat(fuse.Stat):
         self.st_mtime = s.st_mtime
         self.st_ctime = time.mktime(info.date_time + (-1, -1, -1))
 
+
+class RarDirFsFile(object):
+    '''
+        File object created by Fuse when a file is read
+    '''
+
+    def __init__(self, path, flags, *mode):
+        object.__init__(self)
+        accmode = os.O_RDONLY | os.O_WRONLY | os.O_RDWR
+        if flags & accmode != os.O_RDONLY:
+            raise IOError(errno.EROFS, '')
+
+        self.file = None
+
+        if os.path.exists("." + path):
+            self.file = NormalFile(path)
+        else:
+            if not path in self.rarDirFs.vfs:
+                raise IOError(errno.ENOENT, '')
+
+            entry = self.rarDirFs.vfs[path]
+            if entry.rar:
+                if entry.rar_info.compress_type == 0x30:
+                    self.file = UnCompressedRarFile(entry)
+                else:
+                    self.file = CompressedRarFile(entry, self.rarDirFs.cacheManager.get(entry))
+            else:
+                self.file = NormalFile(entry.realpath)
+
+    def read(self, length, offset):
+        return self.file.read(length, offset)
+
+    def flush(self):
+        pass
+
+    def release(self, flags):
+        self.file.close()
+
+class NormalFile(object):
+    '''
+        A "Wrapper" around a normal file
+    '''
+
+    def __init__(self, path):
+        object.__init__(self)
+        self.file = open("." + path, "rb")
+        self.close = self.file.close
+
+    def read(self, length, offset):
+        self.file.seek(offset)
+        return self.file.read(length)
+
+class UnCompressedRarFile(object):
+    '''
+        "Wrapper" around an uncompressed file inside a rar archive
+    '''
+
+    def __init__(self, entry):
+        object.__init__(self)
+        self.rar = entry.rar
+        self.inf = entry.rar_info
+
+    def read(self, length, offset):
+        return self.rar.read_partial(self.inf.filename, offset, length)
+
+    def close(self):
+        pass
+
+class CompressedRarFile(object):
+    '''
+        "Wrapper" around a compressed file inside a rar archive.
+    '''
+
+    def __init__(self, entry, filename):
+        object.__init__(self)
+
+        self.file = open(filename, 'rb')
+
+        self.filename = filename
+        self.close = self.file.close
+        self.real_size = entry.rar_info.file_size
+
+
+    def read(self, length, offset):
+        if offset < self.real_size and offset >= os.path.getsize(self.filename):
+            self.wait(offset, 1) # Wait a second
+        self.file.seek(offset)
+        return self.file.read(length)
+
+    def wait(self, offset, timeout):
+        '''
+            Give unrar some time to fill in some data, up to timeout seconds.
+
+            Raises IOError if timeout seconds has passed and size of file isn't
+            larger the offset.
+        '''
+        delay = timeout/10.0
+        while timeout > 0:
+            time.sleep(delay)
+            timeout -= delay
+
+            if offset < os.path.getsize(self.filename):
+                return
+
+        # Tell application to try again
+        raise IOError(errno.EAGAIN, '')
+
+
 class RarDirFs(fuse.Fuse):
     '''
         Mount a directory read only with the content of rar files display instead.
@@ -135,10 +311,12 @@ class RarDirFs(fuse.Fuse):
         self.flatten = None
         self.srcdir = None
         self.only_first = None
+        self.cache_path = None
+        self.enable_unrar = None
 
         # Use a special class for file operations
-        self.file_class = self.RarDirFsFile
-        self.RarDirFsFile.rarDirFs = self
+        self.file_class = RarDirFsFile
+        self.file_class.rarDirFs = self
 
         self.filterRes = []
         self.flattenRes = []
@@ -229,8 +407,8 @@ class RarDirFs(fuse.Fuse):
         rar = self.rars.get(filename, rarfile.RarFile("." + filename, only_first=self.only_first))
 
         for rar_info in rar.infolist():
-            # Skip compressed files
-            if rar_info.compress_type != 0x30:
+            # Skip compressed files if unrar isn't enabled
+            if rar_info.compress_type != 0x30 and not self.enable_unrar:
                 continue
             # Flatten rar archive
             name = rar_info.filename.split('\\')[-1]
@@ -333,69 +511,16 @@ class RarDirFs(fuse.Fuse):
         return os.statvfs(".")
 
     def fsinit(self):
-        self.filterRes = parsePatternFile(self.filter)
-        self.flattenRes = parsePatternFile(self.flatten)
-        os.chdir(self.srcdir)
-
-    class RarDirFsFile(object):
-        '''
-            File object created by Fuse when a file is read
-        '''
-
-        def __init__(self, path, flags, *mode):
-            accmode = os.O_RDONLY | os.O_WRONLY | os.O_RDWR
-            if flags & accmode != os.O_RDONLY:
-                raise IOError(errno.EROFS, '')
-
-            self.file = None
-
-            if os.path.exists("." + path):
-                self.file = RarDirFs.NormalFile(path)
+        try:
+            self.filterRes = parsePatternFile(self.filter)
+            self.flattenRes = parsePatternFile(self.flatten)
+            os.chdir(self.srcdir)
+            if self.enable_unrar:
+                self.cacheManager = CacheManager(self.cache_path)
             else:
-                if path in self.rarDirFs.vfs:
-                    entry = self.rarDirFs.vfs[path]
-                    if entry.rar:
-                        self.file = RarDirFs.RarFile(entry.rar, entry.rar_info.filename)
-                    else:
-                        self.file = RarDirFs.NormalFile(entry.realpath)
-                else:
-                    raise IOError(errno.ENOENT, '')
+                self.cacheManager = None
+        except Exception, e:
+            print traceback.format_exc()
+            raise IOError(errno.EIO, '')
 
-        def read(self, length, offset):
-            return self.file.read(length, offset)
 
-        def flush(self):
-            pass
-
-        def release(self, flags):
-            self.file.close()
-
-    class NormalFile(object):
-        '''
-            A "Wrapper" around a normal file
-        '''
-
-        def __init__(self, path):
-            self.file = open("." + path, 'r')
-
-        def read(self, length, offset):
-            self.file.seek(offset)
-            return self.file.read(length)
-
-        def close(self):
-            self.file.close()
-
-    class RarFile(object):
-        '''
-            "Wrapper" around the RarFile in entry
-        '''
-
-        def __init__(self, rar, filename):
-            self.rar = rar
-            self.filename = filename
-
-        def read(self, length, offset):
-            return self.rar.read_partial(self.filename, offset, length)
-
-        def close(self):
-            pass
